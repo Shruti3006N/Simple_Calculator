3c70193fe36594484686151d2bb1a3ae21a8c8cf
Shruti Namdeo <75172497+Shruti3006N@users.noreply.github.com>
2021-03-09 22:41:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Enter num1 operator num2 (5*7) : ");
+        Scanner scan = new Scanner(System.in);
+        double d1 = scan.nextDouble();
+        char op = scan.next().charAt(0);
+        double d2 = scan.nextDouble();
+
+        if (op == '+') {
+            System.out.println((int)d1 + d2);
+        }
+        else if (op == '-'){
+            System.out.println((int)d1 - d2);
+        }
+        else if (op == '*'){
+            System.out.println((int)d1 * d2);
+        }
+        else if (op == '/'){
+            System.out.println(d1 / d2);
+        }
+
+    }
+ }
+
